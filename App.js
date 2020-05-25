7f6b823024b8731543545cfed3a7b6354ab2fd98
laranja <laranja1419@gmail.com>
2020-05-25 15:46:01 +0900

    좌표

--- App.js
+++ App.js
@@ -1,19 +1,24 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Alert} from "react-native";
+import Loading from "./Loading";
+import * as Location from "expo-location";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class extends React.Component {
+  getLocation = async() => {
+    try {
+      const response = await Location.requestPermissionsAsync();
+      console.log(response);
+      const location = await Location.getCurrentPositionAsync()
+      console.log(location);
+    } catch (error) {
+      Alert.alert("Can't find you.", "So sad");
+    }
+  };
+  componentDidMount(){
+    this.getLocation();
+  }
+
+  render(){
+    return <Loading />;
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
